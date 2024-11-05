c475cde80b9e030b45f58a7f8b3eaa3b7c2bf73a
Kathrin-llanos <katherinllanos617@gmail.com>
2024-11-04 20:57:02 -0500

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 const routes: Routes = [
-
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'card',
+    component:CardsComponent
+  },
+  {
+    path:'**',
+    redirectTo:'home'
+  }
 ];
-
-
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentsModule } from './components/components.module';
+import { HomeModule } from './home/home.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
